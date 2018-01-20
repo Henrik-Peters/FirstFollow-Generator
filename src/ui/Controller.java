409085c69944fb98e2d0cf57510e3732b9560060
Henrik-Peters <peters-henrik@outlook.de>
2018-01-20 03:34:59 +0100

    Initial commit

---
+++ src/ui/Controller.java
@@ -0,0 +1,9 @@
+// ---------------------------------------------------------------------
+// MIT License
+// Copyright (c) 2018 Henrik Peters
+// See LICENSE file in the project root for full license information.
+// ---------------------------------------------------------------------
+package ui;
+
+public class Controller {
+}
