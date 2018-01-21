fe3bb261234e8df4221e5be176168ffe594a2c44
Henrik-Peters <peters-henrik@outlook.de>
2018-01-21 18:22:26 +0100

    Dynamic adding of cells in the result grid

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -6,12 +6,10 @@
 package ui;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 
 public class Controller {
     public TextArea GrammarBox;
@@ -22,6 +20,71 @@ public class Controller {
     public GridPane FirstFollowGrid;
 
     public void CalculateSets(ActionEvent actionEvent) {
+        ClearGrid(FirstFollowGrid);
         ResultScrollPane.setVisible(true);
+
+        AddHeadlines(FirstFollowGrid, "Symbol", "FIRST", "FOLLOW");
+    }
+
+    private void ClearGrid(GridPane grid) {
+        grid.getChildren().clear();
+        grid.getRowConstraints().clear();
+        grid.getColumnConstraints().clear();
+    }
+
+    private void AddHeadlines(GridPane grid, String... headlines) {
+
+        for (String headline : headlines) {
+            grid.getColumnConstraints().add(new ColumnConstraints(
+                    Control.USE_COMPUTED_SIZE,
+                    Control.USE_COMPUTED_SIZE,
+                    Control.USE_COMPUTED_SIZE,
+                    Priority.SOMETIMES,
+                    HPos.CENTER,
+                    true));
+        }
+
+        grid.getRowConstraints().add(new RowConstraints(
+                Control.USE_COMPUTED_SIZE,
+                Control.USE_COMPUTED_SIZE,
+                Control.USE_COMPUTED_SIZE,
+                Priority.SOMETIMES,
+                VPos.CENTER,
+                true));
+
+        for (int i = 0; i < headlines.length; i++) {
+            Label label = new Label(headlines[i]);
+            label.setMaxWidth(Double.MAX_VALUE);
+            label.getStyleClass().add("grid-cell");
+            label.getStyleClass().add("grid-cell-headline");
+
+            if (i == 0) {
+                label.getStyleClass().add("grid-cell-firstCol");
+            }
+
+            grid.add(label, i, grid.getRowCount() - 1);
+        }
+    }
+
+    private void AddRow(GridPane grid, String... contents) {
+        grid.getRowConstraints().add(new RowConstraints(
+                Control.USE_COMPUTED_SIZE,
+                Control.USE_COMPUTED_SIZE,
+                Control.USE_COMPUTED_SIZE,
+                Priority.SOMETIMES,
+                VPos.CENTER,
+                true));
+
+        for (int i = 0; i < contents.length; i++) {
+            Label label = new Label(contents[i]);
+            label.setMaxWidth(Double.MAX_VALUE);
+            label.getStyleClass().add("grid-cell");
+
+            if (i == 0) {
+                label.getStyleClass().add("grid-cell-firstCol");
+            }
+
+            grid.add(label, i, grid.getRowCount() - 1);
+        }
     }
 }
