fd198816a68f740e32e890a4220c26508377f4d2
Henrik-Peters <peters-henrik@outlook.de>
2018-01-25 19:18:05 +0100

    Added the grammar parser for production rule parsing

--- src/generator/GrammarParser.java
+++ src/generator/GrammarParser.java
@@ -5,7 +5,7 @@
 // ---------------------------------------------------------------------
 package generator;
 
-import java.util.Arrays;
+import java.util.*;
 import static generator.SymbolSet.*;
 import static generator.Singletons.*;
 
@@ -46,8 +46,77 @@ public class GrammarParser {
                 .map(str -> (str.equals("ε")) ? ε : new Terminal(str))
                 .collect(toSymbolSet());
 
+
         //Parse productions
         ProductionSet productions = new ProductionSet();
+
+        //Sort the nonterminals by their length for the longest match
+        Nonterminal[] sortedNonterminals = nonterminals.stream()
+                .sorted((a, b) -> b.text.length() - a.text.length())
+                .toArray(Nonterminal[]::new);
+
+        //Sort the terminals by their length for the longest match
+        Terminal[] sortedTerminals = terminals.stream()
+                .sorted((a, b) -> b.text.length() - a.text.length())
+                .toArray(Terminal[]::new);
+
+        for (String line : lines) {
+            String[] lineSplit = line.split("->");
+            String[] subRuleSplit = lineSplit[1].split("\\|");
+
+            Nonterminal leftSide = nonterminals.stream()
+                    .filter(n -> n.text.equals(lineSplit[0].trim()))
+                    .findFirst()
+                    .map(Nonterminal.class::cast)
+                    .orElse(null);
+
+            for (String subRule : subRuleSplit) {
+                List<Symbol> rightSide = new LinkedList<>();
+                String restRule = subRule.trim();
+
+                while (!restRule.isEmpty()) {
+                    boolean foundNonterminal = false;
+
+                    //Try to match with the longest nonterminal first
+                    for (Nonterminal n : sortedNonterminals) {
+
+                        if (restRule.length() >= n.text.length() &&
+                            n.text.equals(restRule.substring(0, n.text.length()))) {
+
+                            rightSide.add(n);
+                            restRule = restRule.substring(n.text.length()).trim();
+                            foundNonterminal = true;
+                            break;
+                        }
+                    }
+
+                    //Try to match with the longest terminal
+                    if (!foundNonterminal) {
+                        for (Terminal t : sortedTerminals) {
+
+                            if (restRule.charAt(0) == 'ε') {
+                                rightSide.add(ε);
+                                restRule = restRule.substring(1);
+                                break;
+                            }
+
+                            if (t != ε &&
+                                restRule.length() >= t.text.length() &&
+                                t.text.equals(restRule.substring(0, t.text.length()))) {
+                                rightSide.add(t);
+                                restRule = restRule.substring(t.text.length()).trim();
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (!rightSide.isEmpty()) {
+                    Word rightWord = new Word(rightSide.toArray(new Symbol[0]));
+                    productions.add(new Production(leftSide, rightWord));
+                }
+            }
+        }
 
         //Find the start symbol
         String startText = lines[0].split("->")[0].trim();
