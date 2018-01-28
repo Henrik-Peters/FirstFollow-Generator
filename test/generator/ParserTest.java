5244e154988290dd4ec228bc55aabba695565b08
Henrik-Peters <peters-henrik@outlook.de>
2018-01-28 18:57:13 +0100

    Extended the grammar parser tests

--- test/generator/ParserTest.java
+++ test/generator/ParserTest.java
@@ -67,6 +67,31 @@ class ParserTest {
     }
 
     @Test
+    void testCombinedMultilines() {
+        String[] lines = new String[]{
+                "S -> A B",
+                "A -> A | a",
+                "B -> B c",
+                "A -> S B | b",
+                "B -> c"
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A, B}, {a, c, b}, {S -> A B, A -> A, A -> a, B -> B c, A -> S B, A -> b, B -> c}, S)", g.toString());
+    }
+
+    @Test
+    void testRedudantMultilines() {
+        String[] lines = new String[]{
+                "S -> A",
+                "A -> a",
+                "A -> a",
+                "A -> ab"
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A}, {a, ab}, {S -> A, A -> a, A -> ab}, S)", g.toString());
+    }
+
+    @Test
     void testMultiCharacterTerminals() {
         String[] lines = new String[]{ "S -> abc" };
         Grammar g = ParseGrammar(lines);
@@ -104,5 +129,73 @@ class ParserTest {
         };
         Grammar g = ParseGrammar(lines);
         assertEquals("({S, A, ABC, AB}, {a}, {S -> A, S -> AB, S -> ABC, A -> AB, ABC -> a, AB -> ABC}, S)", g.toString());
+    }
+
+    @Test
+    void testMissingWhitespaces() {
+        String[] lines = new String[]{
+                "S -> AB C",
+                "A -> a",
+                "B -> b",
+                "C -> c"
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A, B, C}, {a, b, c}, {S -> A B C, A -> a, B -> b, C -> c}, S)", g.toString());
+    }
+
+    @Test
+    void testMissingTerminalWhitespaces() {
+        String[] lines = new String[]{
+                "S -> AB | ABc",
+                "A -> a",
+                "AB -> b"
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A, AB}, {c, a, b}, {S -> AB, S -> AB c, A -> a, AB -> b}, S)", g.toString());
+    }
+
+    @Test
+    void testLeftSideWhitespaces() {
+        String[] lines = new String[]{
+                "S  -> A AB",
+                "  A -> a",
+                "  AB     -> b"
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A, AB}, {a, b}, {S -> A AB, A -> a, AB -> b}, S)", g.toString());
+    }
+
+    @Test
+    void testRightSideWhitespaces() {
+        String[] lines = new String[]{
+                "S ->  A     AB ",
+                "A ->     a",
+                "AB-> b   "
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A, AB}, {a, b}, {S -> A AB, A -> a, AB -> b}, S)", g.toString());
+    }
+
+    @Test
+    void testBothSidedWhitespaces() {
+        String[] lines = new String[]{
+                "S  ->  A     AB ",
+                "  A ->     a",
+                "  AB->     b   "
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A, AB}, {a, b}, {S -> A AB, A -> a, AB -> b}, S)", g.toString());
+    }
+
+    @Test
+    void testEmptyLines() {
+        String[] lines = new String[]{
+                "S -> A",
+                "",
+                "A -> a",
+                ""
+        };
+        Grammar g = ParseGrammar(lines);
+        assertEquals("({S, A}, {a}, {S -> A, A -> a}, S)", g.toString());
     }
 }
